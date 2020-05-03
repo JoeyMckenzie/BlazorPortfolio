89cd6379bbeb053a0459ed3b87c3f65080e3c474
Joey Mckenzie <joey.mckenzie27@gmail.com>
2020-05-03 14:38:19 -0700

    chore(Articles): Add Blazor article, spell check

--- Services/BlogPostService.cs
+++ Services/BlogPostService.cs
@@ -8,6 +8,7 @@ namespace Portfolio.Services
     {
         private readonly static IEnumerable<BlogPostViewModel> _blogPosts = new List<BlogPostViewModel>
         {
+            new BlogPostViewModel("Blazor Basics - Consuming External APIs from the Frontend", "https://betweentwobrackets.netlify.app/2020-04-24-blazor-basics-calling-external-apis/", new DateTime(2020, 4, 24)),
             new BlogPostViewModel(".NET Core, Dapper, MediatR, and Buzzword Bingo (Part 4)", "https://betweentwobrackets.netlify.app/2020-02-02-net-core-dapper-and-crud-buzzword-bingo-part-4/", new DateTime(2020, 2, 20)),
             new BlogPostViewModel(".NET Core, Dapper, MediatR, and Buzzword Bingo (Part 3)", "https://betweentwobrackets.netlify.app/2019-11-19-net-core-dapper-and-crud-buzzword-bingo-part-3/", new DateTime(2019, 11, 19)),
             new BlogPostViewModel("A Tale of Two Types - C# 8.0, Nullable References, and Designing with Intent", "https://betweentwobrackets.netlify.app/2019-11-05-csharp-8-nullable-reference-types/", new DateTime(2019, 11, 5)),
